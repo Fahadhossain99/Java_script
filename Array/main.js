43621ef89279aad20a5bee911cb9998f736374bc
Fahad Hossain <64582073+Fahadhossain99@users.noreply.github.com>
2022-02-21 10:04:22 +0600

    Array function

---
+++ Array/main.js
@@ -0,0 +1,21 @@
+import { concat } from "core-js/core/array";
+
+var name1,name2,name3,name4,name5;
+name1="fahad";
+name2="sharna";
+name3="sanjida";
+name4="hossain";
+name5="inaya";
+console.log(name5);
+
+var names=["fahad","sharna","sanjida","hossain","inaya"];
+console.log(names.length);
+names.push("shaika");
+console.log(names.length);
+names.pop();
+console.log(names);
+names.pop();
+console.log(names);
+names.pop();
+console.log(names);
+
---
+++ array methods  find,findIndex/main.js
@@ -0,0 +1,4 @@
+let numbers =[5,2,7,9,3,9]
+let firstEvenNumbers = numbers.find(x => x%2===0)
+console.log(firstEvenNumbers)
+
